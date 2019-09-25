e60976867d7f532a1e5570ce9899cb8c048129d9
Franco Castellano <fcastellano@belatrixsf.com>
2019-09-25 09:48:48 -0500

    Project created

---
+++ Zomato API ExampleTests/Zomato_API_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  Zomato_API_ExampleTests.swift
+//  Zomato API ExampleTests
+//
+//  Created by Franco Castellano Alvarez on 9/25/19.
+//  Copyright © 2019 Castellano. All rights reserved.
+//
+
+import XCTest
+@testable import Zomato_API_Example
+
+class Zomato_API_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
